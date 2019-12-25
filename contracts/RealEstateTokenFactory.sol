08510047e07150ebeaa2c54f6618423e7c90d54d
erwindeg <erwin.degier@trifork.nl>
2019-12-25 11:16:16 +0100

    added comments

--- contracts/RealEstateTokenFactory.sol
+++ contracts/RealEstateTokenFactory.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.5.0;
 
 import "./RealEstateToken.sol";
-
+/*
+* The factory is used to allow anyone to deploy a RealEstateToken instance
+* and become the owner without owning the source. This also makes the deployment easier as
+* the deploy script automatically assigns the deployer of the Factory as the owner of the * * RealEstateTokenInstance
+*/
 contract RealEstateTokenFactory {
     address[] public deployedRealEstateTokens;
 
